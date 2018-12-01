8fe70ef5bbbe3710c067f246ea89068444063532
Aleksandr <chy4egg@gmail.com>
2018-12-01 19:37:00 +0300

    first_commit

---
+++ controller.js
@@ -0,0 +1,8 @@
+document.onkeydown = function(e) {
+  var keys = {
+    37: left,
+    38: up,
+    40: down,
+    39: right
+  }
+};
---
+++ snake.js
@@ -0,0 +1,6 @@
+//here is out code
+;(function(){
+
+  console.log('here is our code!!!! lololo');
+
+}());
